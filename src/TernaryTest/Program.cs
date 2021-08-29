90b2bf6b06e94a09fdeab77e5fb6e887acc48b3f
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-29 12:10:15 +0800

    feat: Add basic project files

---
+++ src/TernaryTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TernaryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
