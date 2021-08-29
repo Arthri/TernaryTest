a0b6ffaac86224883810e91c7dda93556ab8e9bb
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-29 12:43:33 +0800

    feat: Update errorneous code

--- src/TernaryTest/Program.cs
+++ src/TernaryTest/Program.cs
@@ -2,18 +2,24 @@
 
 namespace TernaryTest
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            string response = "something so compiler doesn't optimize away";
-            Console.WriteLine("Some other thing | " + response);
             var input = Console.ReadLine();
-            response = input == "Y"
-                ? "Success!"
-                : input == "N"
-                    ? "Failed"
-                    : "Invalid response";
+            string response;
+            if (input == "Y")
+            {
+                response = "Success!";
+            }
+            else if (input == "N")
+            {
+                response = "Failed";
+            }
+            else
+            {
+                response = "Invalid response";
+            }
             Console.WriteLine(response);
         }
     }
